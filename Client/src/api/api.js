974818c4a86b4cfcc9142675d9d9677954c9287e
Rahul123-debug539 <rtiwari23763@gmail.com>
2025-12-07 14:35:37 +0530

    Fix Vercel routing for admin create page

--- Client/src/api/api.js
+++ Client/src/api/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+const API = axios.create({
+  baseURL: "https://elearn-70zx.onrender.com"
 });
 
 // REQUEST: automatically attach token
