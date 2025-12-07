af9aa01371bb36d78a413199b214bdf6ee59c75b
Rahul123-debug539 <rtiwari23763@gmail.com>
2025-12-07 18:36:09 +0530

    update

--- Client/src/pages/admin/ManageTopics.jsx
+++ Client/src/pages/admin/ManageTopics.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../../api/api";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 function ManageTopics() {
   const [categories, setCategories] = useState([]);
