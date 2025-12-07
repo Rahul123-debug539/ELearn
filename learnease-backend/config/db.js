7c401e8d18f75d801561fa2b3bf77b1645827711
Rahul123-debug539 <rtiwari23763@gmail.com>
2025-12-07 13:12:38 +0530

    .env

--- learnease-backend/config/db.js
+++ learnease-backend/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    mongoose.connect(process.env.MONGO_URI)
+
     console.log("🔥 MongoDB Connected Successfully");
   } catch (err) {
     console.error("❌ MongoDB Connection Failed:", err.message);
