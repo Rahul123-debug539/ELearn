3d55a0938ce6d7a36fa91db167faa94dd7f8475b
Rahul123-debug539 <rtiwari23763@gmail.com>
2026-01-03 18:57:03 +0530

    backend fixed

--- learnease-backend/controllers/authController.js
+++ learnease-backend/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const generateOTP = require("../utils/generateOTP");
-const transporter = require("../config/mailer");
+const transporter = require("../config/mail");
 
 
 // REGISTER
