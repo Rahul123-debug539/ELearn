9f38fe64934e2edb3f02458ebb5b66305c2783d8
Rahul123-debug539 <rtiwari23763@gmail.com>
2026-06-11 22:37:30 +0530

    chaanged backend Api

--- Client/src/api/api.js
+++ Client/src/api/api.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 // ✅ Create API instance
-// const api = axios.create({
-//   baseURL: "https://elearn-70zx.onrender.com"
-// });
-
 const api = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://elearn-70zx.onrender.com"
 });
+
+
 
 
 // ✅ REQUEST: automatically attach token
